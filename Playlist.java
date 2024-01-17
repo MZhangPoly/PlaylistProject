c2615f04e79fbb146f52cc20ada15522aa2b475f
Michael Zhang <mzhang26@students.polytechnic.org>
2024-01-17 10:21:17 -0800

    added constructor

--- Playlist.java
+++ Playlist.java
@@ -1,24 +1,11 @@
 import java.util.ArrayList;
 
-/**
- * The Playlist class, which will keep track of a playlist of Song objects
- * Refer to the project description to make sure you have access to all available methods
- */
 public class Playlist { 
-    /**
-     * Fields-- This will likely just need to be the ArrayList of Songs. Reference our previous problems
-     * (CarDealership, Zoo) for structure on how this will look
-     */
+    ArrayList<Song> playlist;
 
-
-
-
-     /**
-      * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
-      * then use additional methods to add Songs in one-by-one
-      */
-
-
+    public Playlist() {
+       playlist = new ArrayList<Song>();
+    }
       
 
       /**
